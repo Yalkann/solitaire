52df1d4f8abba22f828602cc1459a3e5cb81d85c
Yalkann <lanneau.t@free.fr>
2022-11-22 02:53:21 +0100

    init machine learning class

---
+++ src/AI_ML.py
@@ -0,0 +1,19 @@
+from src.AI import *
+
+
+class AI_ML(AI):
+    def __init__(self):
+        """
+        inputs:
+
+        -topCardRevealed: -1/1
+        -foundation: 4 variables containing the top card mapped from 1 to 52 and 0 for empty spots
+        -table: 7 by 18 table containing the cards on the table mapped from 1 to 52 or -1 for hidden cards and 0 for empty spots
+        -stock
+
+
+        """
+        pass
+
+    def getAction(self, game: Game):
+        pass
